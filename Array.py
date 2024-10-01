00228df30160393170e5a3f840cbbb2c5f10238c
Gavin Zeager <64079569+XHiddenProjects@users.noreply.github.com>
2024-10-01 08:33:09 -0400

    Update Array.py

--- Array.py
+++ Array.py
@@ -14,18 +14,24 @@ class Array:
         """
         return sorted(arr,reverse=reverse)
     
-    def dup(self, arr: List[Union[int,str]], action: str='FILTER')-> List[Union[list,int]]:
+    def dup(self, arr: List[Union[int,str]], repeat: int=1, action: str='FILTER')-> List[Union[list,int]]:
         """Manipulates duplicates in the list
 
         Args:
             arr (List[Union[int,str]]): Array to update
+            repeat (int): Restricts how many times the number can repeat. Default to 1 
             action (str, optional): Dictates on what the output should be. Options: \"FILTER\" | \"GET\". Defaults to 'FILTER'.
 
         Returns:
             List[Union[list,int]]: The updated array or the length of the updated array
         """
+        updated_arr = []
         if action.upper()=='FILTER':
-            arr = list(set(arr))
+            for x in range(len(arr)):
+                if(updated_arr.count(arr[x]) < repeat): updated_arr.append(arr[x])
+            arr = updated_arr
         else:
-            arr = len(list(set(arr)))
+            for x in range(len(arr)):
+                if(updated_arr.count(arr[x]) < repeat): updated_arr.append(arr[x])
+            arr = len(updated_arr)
         return arr
