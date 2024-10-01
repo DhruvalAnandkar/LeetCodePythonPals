01db0f604da6eaf7892a4b56be8f01db8059176d
Gavin Zeager <64079569+XHiddenProjects@users.noreply.github.com>
2024-10-01 08:14:33 -0400

    Update Array.py

--- Array.py
+++ Array.py
@@ -2,16 +2,33 @@ from typing import List, Union
 class Array:
     """Modifies the List objects
     """
-    def sort(self, nums: List[Union[int,str]], reverse: bool=False)->List[Union[int,str]]:
+    def sort(self, arr: List[Union[int,str]], reverse: bool=False)->List[Union[int,str]]:
         """Sorts the array in ascending/descending order
 
         Args:
-            nums (List[Union[int,str]]): A array object to sort out
+            arr (List[Union[int,str]]): A array object to sort out
             reverse (bool, optional): Reverses the order. Defaults to False.
 
         Returns:
             List[Union[int,str]]: Returns the updated ordered list
         """
-        return sorted(nums,reverse=reverse)
-arr = Array()
-print(arr.sort([0,5,2,7,8],False))
+        return sorted(arr,reverse=reverse)
+    
+    def dup(self, arr: List[Union[int,str]], action: str='FILTER')-> List[Union[list,int]]:
+        """Manipulates duplicates in the list
+
+        Args:
+            arr (List[Union[int,str]]): Array to update
+            action (str, optional): Dictates on what the output should be. Options: \"FILTER\" | \"GET\". Defaults to 'FILTER'.
+
+        Returns:
+            List[Union[list,int]]: The updated array or the length of the updated array
+        """
+        if action.upper()=='FILTER':
+            arr = list(set(arr))
+        else:
+            arr = len(list(set(arr)))
+        return arr
+        
+# Test executer
+print(Array().dup([1,1,2],'FILTER'))
